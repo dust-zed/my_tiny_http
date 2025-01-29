464349469cc948d1beb65ad7cc35a5034f13abce
dust-zed <1186669164@qq.com>
2025-01-29 13:45:33 +0800

    连接(Connection)的实现

--- src/connection.rs
+++ src/connection.rs
@@ -1,6 +1,187 @@
-use std::net::SocketAddr;
+use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream, ToSocketAddrs};
+use std::os::unix::net::{self as unix_net, UnixStream};
+use std::path::PathBuf;
+
+pub enum Listener {
+    Tcp(TcpListener),
+    #[cfg(unix)]
+    Unix(unix_net::UnixListener),
+}
+
+impl Listener {
+    pub(crate) fn local_addr(&self) -> std::io::Result<ListenAddr> {
+        match self {
+            Self::Tcp(l) => l.local_addr().map(ListenAddr::from),
+            Self::Unix(l) => l.local_addr().map(ListenAddr::from),
+        }
+    }
+
+    pub(crate) fn accept(&self) -> std::io::Result<(Connection, Option<SocketAddr>)> {
+        match self {
+            Self::Tcp(l) => l
+                .accept()
+                .map(|(conn, addr)| (Connection::from(conn), Some(addr))),
+
+            Self::Unix(l) => l.accept().map(|(conn, _)| (Connection::from(conn), None)),
+        }
+    }
+}
+
+impl From<TcpListener> for Listener {
+    fn from(l: TcpListener) -> Self {
+        Listener::Tcp(l)
+    }
+}
+
+impl From<unix_net::UnixListener> for Listener {
+    fn from(l: unix_net::UnixListener) -> Self {
+        Listener::Unix(l)
+    }
+}
+
+/// Unified connection. Either a [`TcpStream`] or [`std::os::unix::net::UnixStream`].
+pub(crate) enum Connection {
+    Tcp(TcpStream),
+    #[cfg(unix)]
+    Unix(unix_net::UnixStream),
+}
+
+impl std::io::Write for Connection {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            Self::Tcp(s) => s.write(buf),
+            #[cfg(unix)]
+            Self::Unix(s) => s.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self {
+            Self::Tcp(s) => s.flush(),
+            Self::Unix(s) => s.flush(),
+        }
+    }
+}
+
+impl std::io::Read for Connection {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        match self {
+            Self::Tcp(s) => s.read(buf),
+            #[cfg(unix)]
+            Self::Unix(s) => s.read(buf),
+        }
+    }
+}
+
+impl Connection {
+    ///get the peer address.Some for Tcp, None for Unix
+    pub(crate) fn peer_addr(&mut self) -> std::io::Result<Option<SocketAddr>> {
+        match self {
+            Self::Tcp(s) => s.peer_addr().map(Some),
+            #[cfg(unix)]
+            Self::Unix(_) => Ok(None),
+        }
+    }
+
+    pub(crate) fn shutdown(&self, how: Shutdown) -> std::io::Result<()> {
+        match self {
+            Self::Tcp(s) => s.shutdown(how),
+            #[cfg(unix)]
+            Self::Unix(s) => s.shutdown(how),
+        }
+    }
+
+    pub(crate) fn try_clone(&self) -> std::io::Result<Self> {
+        match self {
+            Self::Tcp(s) => s.try_clone().map(Self::from),
+            #[cfg(unix)]
+            Self::Unix(s) => s.try_clone().map(Self::from),
+        }
+    }
+}
+
+impl From<TcpStream> for Connection {
+    fn from(stream: TcpStream) -> Self {
+        Self::Tcp(stream)
+    }
+}
+
+#[cfg(unix)]
+impl From<UnixStream> for Connection {
+    fn from(stream: UnixStream) -> Self {
+        Self::Unix(stream)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ConfigListenAddr {
+    IP(Vec<SocketAddr>),
+    #[cfg(unix)]
+    Unix(std::path::PathBuf),
+}
+
+impl ConfigListenAddr {
+    pub fn from_socket_addrs<A: ToSocketAddrs>(addrs: A) -> std::io::Result<Self> {
+        addrs.to_socket_addrs().map(|it| Self::IP(it.collect()))
+    }
+
+    #[cfg(unix)]
+    pub fn unix_from_path<P: Into<PathBuf>>(path: P) -> Self {
+        Self::Unix(path.into())
+    }
+
+    pub(crate) fn bind(&self) -> std::io::Result<Listener> {
+        match self {
+            Self::IP(a) => TcpListener::bind(a.as_slice()).map(Listener::from),
+            #[cfg(unix)]
+            Self::Unix(a) => unix_net::UnixListener::bind(a).map(Listener::from),
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum ListenAddr {
     IP(SocketAddr),
+    #[cfg(unix)]
+    Unix(unix_net::SocketAddr),
+}
+
+impl ListenAddr {
+    pub fn to_ip(self) -> Option<SocketAddr> {
+        match self {
+            Self::IP(addr) => Some(addr),
+            #[cfg(unix)]
+            Self::Unix(_) => None,
+        }
+    }
+
+    #[cfg(unix)]
+    pub fn to_unix(self) -> Option<unix_net::SocketAddr> {
+        match self {
+            Self::IP(_) => None,
+            Self::Unix(addr) => Some(addr),
+        }
+    }
+}
+
+impl From<SocketAddr> for ListenAddr {
+    fn from(addr: SocketAddr) -> Self {
+        Self::IP(addr)
+    }
+}
+
+impl From<unix_net::SocketAddr> for ListenAddr {
+    fn from(addr: unix_net::SocketAddr) -> Self {
+        Self::Unix(addr)
+    }
+}
+
+impl std::fmt::Display for ListenAddr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IP(addr) => addr.fmt(f),
+            #[cfg(unix)]
+            Self::Unix(addr) => std::fmt::Debug::fmt(addr, f),
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-use std::sync::{atomic::AtomicBool, Arc};
+use connection::{ConfigListenAddr, ListenAddr};
+use std::error::Error;
 use std::io::Error as IoError;
+use std::net::ToSocketAddrs;
+use std::sync::{atomic::AtomicBool, Arc};
 use util::messages_queue::MessageQueue;
-use connection::ListenAddr;
-
 
 pub use request::Request;
 
+mod connection;
 mod request;
 mod util;
-mod connection;
 
 pub struct Server {
-
     //只要服务还存在就为false
     //设置为true时，所有子任务将在几百毫秒内关闭
     close: Arc<AtomicBool>,
@@ -20,12 +20,12 @@ pub struct Server {
     messages: Arc<MessageQueue<Message>>,
 
     //result for TcpListener::local_addr()
-    listeninng_port: ListenAddr
+    listeninng_port: ListenAddr,
 }
 
 enum Message {
     Error(IoError),
-    NewRequest(Request)
+    NewRequest(Request),
 }
 
 impl From<IoError> for Message {
@@ -34,9 +34,47 @@ impl From<IoError> for Message {
     }
 }
 
+impl From<Request> for Message {
+    fn from(req: Request) -> Self {
+        Message::NewRequest(req)
+    }
+}
+
+pub struct IncomingRequests<'a> {
+    server: &'a Server,
+}
+
+impl Server {
+    pub fn http<A>(addr: A) -> Result<Server, Box<dyn Error + Send + Sync + 'static>>
+    where
+        A: ToSocketAddrs,
+    {
+
+    }
+
+    ///builds a new server that listens on specific address
+    pub fn new(config: ServerConfig) -> Result<Server, Box<dyn Error + Send + Sync + 'static>> {
+        let listener = config.addr.bind()?;
+        
+    }
+}
+
+pub struct ServerConfig {
+    //the address try to listen to
+    pub addr: ConfigListenAddr,
+
+    //if `Some``, then the server will use SSL to encode the communications
+    pub ssl: Option<SslConfig>,
+}
+
+pub struct SslConfig {
+    ///contatains the public certifications to send to client
+    pub certificate: Vec<u8>,
+    ///contains the ultra-secret private key used to decode communications
+    pub private_key: Vec<u8>,
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 }
